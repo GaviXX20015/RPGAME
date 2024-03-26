be899cdd90470245aba16a9374553ff51114957b
GaviXX20015 <medake28@gmail.com>
2024-03-26 22:13:37 +0000

    adding the twitter link to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -31,6 +31,11 @@ const Footer = () => {
             classes.links + " col-lg-4 col-12 d-flex justify-content-center"
           }
         >
+          <li className={classes.icon + " me-3"}>
+            <a target="_blank" href=" https://twitter.com/RPGonSolana">
+              <i class="bi bi-twitter-x"></i>
+            </a>
+          </li>
           <li className={classes.icon}>
             <a target="_blank" href=" https://telegram.me/RPGonSolanaBot">
               <i class="bi bi-telegram"></i>
